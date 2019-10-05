6c60a6ebe739310c9797eb83e707d914bb48f089
Adam Spooner <adam@keyba.se>
2019-10-05 09:42:58 -0400

    Fix style PropType.

--- src/index.js
+++ src/index.js
@@ -192,7 +192,7 @@ Lottie.propTypes = {
   ariaRole: PropTypes.string,
   ariaLabel: PropTypes.string,
   title: PropTypes.string,
-  style: PropTypes.string,
+  style: PropTypes.object,
 };
 
 Lottie.defaultProps = {
